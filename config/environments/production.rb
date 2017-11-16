4493abdec57f9eb8c022a078f343d0049dff9352
Ingrid Molloy <ingrid.molloy@shopify.com>
2017-11-16 13:58:11 -0800

    installed devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Update this with the correct env info for production
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
